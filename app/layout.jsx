12881306cba5c438711a48eefb3617fadc2298bc
Nick Kwiat <nickkwiat@hotmail.com>
2024-06-11 17:03:38 -0400

    Routes and Favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
 import TopBar from "./components/TopBar";
 import "./globals.scss";
-
+// import favicon from "./favicon.ico"
 
 export const metadata = {
   title: "Pan's Haven",
   description: "For all companions",
+  // icon: favicon
 };
 
 export default function RootLayout({ children }) {
