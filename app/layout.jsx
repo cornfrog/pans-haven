ae01a67739673ef8a3be305172983122cb4a40dc
Cornfrog <iamcornfrog@gmail.com>
2024-06-11 16:15:05 -0400

    starting structure for topbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import TopBar from "./components/TopBar";
 import "./globals.scss";
 
 
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <TopBar />
         {children}
       </body>
     </html>
